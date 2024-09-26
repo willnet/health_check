7e7fa5b7d7835e25155103ee40300fb8f25f0450
Shinichi Maeshima <netwillnet@gmail.com>
2024-09-26 17:16:35 +0900

    /migrationのテストを追加した
    もともとのテストは
    
    1. migrationファイルがない
    2. migrationファイルがあり、migration未実施
    3. migraitionファイルが有り、途中までmigration済み
    4. 全部migration済み
    
    という4パターンでテストをしているようだった。しかしRails4以降はRailsの`ActiveRecord::Migration.check_pending!`を使っているため、全パターンは必要なさそう(Railsの機能のテストになってしまう)だったので1,2,4だけをテストすることにした

--- spec/health_check_spec.rb
+++ spec/health_check_spec.rb
@@ -7,6 +7,35 @@ RSpec.describe HealthCheck, type: :request do
   end
 
   context '/migration' do
+    after do
+      Dir.glob('spec/dummy/db/migrate/*').each do |f|
+        FileUtils.rm(f)
+      end
+      FileUtils.cd(FakeApp.config.root) do
+        ActiveRecord::Tasks::DatabaseTasks.drop_current
+      end
+    end
 
+    it 'works with no migration files' do
+      get '/health_check/migration'
+      expect(response).to be_ok
+    end
+
+    it 'fails with pending migration files ' do
+      FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
+      FileUtils.cd(FakeApp.config.root) do
+        get '/health_check/migration'
+      end
+      expect(response.status).to eq(550)
+    end
+
+    it 'works with applied migration files ' do
+      FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
+      FileUtils.cd(FakeApp.config.root) do
+        ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrate
+        get '/health_check/migration'
+      end
+      expect(response).to be_ok
+    end
   end
 end
