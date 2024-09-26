6cd9539cce8dbd4ece2537810d49ce7428d185c5
Shinichi Maeshima <netwillnet@gmail.com>
2024-09-26 18:54:51 +0900

    /databaseに対応した
    DBの接続が残っているとキャッシュを利用するようで期待通り/databaseが550を返さなかったので明示的にdisconenctしている

--- spec/health_check_spec.rb
+++ spec/health_check_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe HealthCheck, type: :request do
       expect(response).to be_ok
     end
 
-    it 'fails with pending migration files ' do
+    it 'fails with pending migration files' do
       FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
       FileUtils.cd(FakeApp.config.root) do
         get '/health_check/migration'
@@ -29,13 +29,46 @@ RSpec.describe HealthCheck, type: :request do
       expect(response.status).to eq(550)
     end
 
-    it 'works with applied migration files ' do
+    it 'works with applied migration files' do
       FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
       FileUtils.cd(FakeApp.config.root) do
         ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrate
         get '/health_check/migration'
       end
       expect(response).to be_ok
+    end
+  end
+
+  describe '/database' do
+    after do
+      Dir.glob('spec/dummy/db/migrate/*').each do |f|
+        FileUtils.rm(f)
+      end
+      FileUtils.cd(FakeApp.config.root) do
+        ActiveRecord::Tasks::DatabaseTasks.drop_current
+      end
+    end
+
+    it 'works with no database' do
+      get '/health_check/database'
+      expect(response).to be_ok
+    end
+
+    it 'works with valid database' do
+      FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
+      FileUtils.cd(FakeApp.config.root) do
+        ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrate
+        get '/health_check/database'
+      end
+      expect(response).to be_ok
+    end
+
+    it 'fails with invalid database' do
+      ActiveRecord::Tasks::DatabaseTasks.migration_connection.disconnect!
+      Rails.root.join('db/test.sqlite3').write('invalid')
+      get '/health_check/database'
+      expect(response.status).to eq(550)
+      expect(response.body).to include 'health_check failed'
     end
   end
 end
