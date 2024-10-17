6661ad0a614177d4db8c76a16b961edc79b941c0
Shinichi Maeshima <netwillnet@gmail.com>
2024-10-17 19:18:57 +0900

    テスト足した
    あと認証系がおわればゴールできる？

--- spec/health_check_spec.rb
+++ spec/health_check_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe HealthCheck, type: :request do
-  context '/health_check' do
+  context '/custom_route_prefix' do
     it 'works with smtp server and valid custom_check' do
       enable_custom_check do
         start_smtp_server do
-          get '/health_check'
+          get '/custom_route_prefix'
           expect(response).to be_ok
         end
       end
@@ -13,7 +13,7 @@ RSpec.describe HealthCheck, type: :request do
 
     it 'fails with no smtp server and valid custom_check' do
       enable_custom_check do
-        get '/health_check'
+        get '/custom_route_prefix'
         expect(response.status).to eq(550)
         expect(response.body).to include 'health_check failed'
       end
@@ -21,18 +21,18 @@ RSpec.describe HealthCheck, type: :request do
 
     it 'fails with smtp server and invalid custom_check' do
       start_smtp_server do
-        get '/health_check'
+        get '/custom_route_prefix'
         expect(response.status).to eq(550)
         expect(response.body).to include 'health_check failed'
       end
     end
   end
 
-  context '/health_check/all' do
+  context '/custom_route_prefix/all' do
     it 'works with smtp server and valid custom_check' do
       enable_custom_check do
         start_smtp_server do
-          get '/health_check/all'
+          get '/custom_route_prefix/all'
           expect(response).to be_ok
         end
       end
@@ -40,7 +40,7 @@ RSpec.describe HealthCheck, type: :request do
 
     it 'fails with no smtp server and valid custom_check' do
       enable_custom_check do
-        get '/health_check/all'
+        get '/custom_route_prefix/all'
         expect(response.status).to eq(550)
         expect(response.body).to include 'health_check failed'
       end
@@ -48,14 +48,14 @@ RSpec.describe HealthCheck, type: :request do
 
     it 'fails with smtp server and invalid custom_check' do
       start_smtp_server do
-        get '/health_check/all'
+        get '/custom_route_prefix/all'
         expect(response.status).to eq(550)
         expect(response.body).to include 'health_check failed'
       end
     end
   end
 
-  context '/health_check/migration' do
+  context '/custom_route_prefix/migration' do
     after do
       Dir.glob('spec/dummy/db/migrate/*').each do |f|
         FileUtils.rm(f)
@@ -66,14 +66,14 @@ RSpec.describe HealthCheck, type: :request do
     end
 
     it 'works with no migration files' do
-      get '/health_check/migration'
+      get '/custom_route_prefix/migration'
       expect(response).to be_ok
     end
 
     it 'fails with pending migration files' do
       FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
       FileUtils.cd(FakeApp.config.root) do
-        get '/health_check/migration'
+        get '/custom_route_prefix/migration'
       end
       expect(response.status).to eq(550)
     end
@@ -82,13 +82,13 @@ RSpec.describe HealthCheck, type: :request do
       FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
       FileUtils.cd(FakeApp.config.root) do
         ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrate
-        get '/health_check/migration'
+        get '/custom_route_prefix/migration'
       end
       expect(response).to be_ok
     end
   end
 
-  describe '/health_check/database' do
+  describe '/custom_route_prefix/database' do
     after do
       Dir.glob('spec/dummy/db/migrate/*').each do |f|
         FileUtils.rm(f)
@@ -99,7 +99,7 @@ RSpec.describe HealthCheck, type: :request do
     end
 
     it 'works with no database' do
-      get '/health_check/database'
+      get '/custom_route_prefix/database'
       expect(response).to be_ok
     end
 
@@ -107,7 +107,7 @@ RSpec.describe HealthCheck, type: :request do
       FileUtils.cp('spec/fixtures/migrate/9_create_countries.rb', 'spec/dummy/db/migrate/')
       FileUtils.cd(FakeApp.config.root) do
         ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrate
-        get '/health_check/database'
+        get '/custom_route_prefix/database'
       end
       expect(response).to be_ok
     end
@@ -115,36 +115,36 @@ RSpec.describe HealthCheck, type: :request do
     it 'fails with invalid database' do
       ActiveRecord::Tasks::DatabaseTasks.migration_connection.disconnect!
       Rails.root.join('db/test.sqlite3').write('invalid')
-      get '/health_check/database'
+      get '/custom_route_prefix/database'
       expect(response.status).to eq(550)
       expect(response.body).to include 'health_check failed'
     end
   end
 
-  describe '/health_check/email' do
+  describe '/custom_route_prefix/email' do
     it 'works with smtp server' do
       start_smtp_server do
-        get '/health_check/email'
+        get '/custom_route_prefix/email'
         expect(response).to be_ok
       end
     end
 
     it 'fails with no smtp server' do
-      get '/health_check/email'
+      get '/custom_route_prefix/email'
       expect(response.status).to eq(550)
       expect(response.body).to include 'health_check failed'
     end
   end
 
-  describe '/health_check/pass (a custom check does nothing)' do
+  describe '/custom_route_prefix/pass (a custom check does nothing)' do
     it 'works if another custom check is invalid' do
-      get '/health_check/pass'
+      get '/custom_route_prefix/pass'
       expect(response).to be_ok
     end
 
     it 'works if another custom check is valid' do
       enable_custom_check do
-        get '/health_check/pass'
+        get '/custom_route_prefix/pass'
         expect(response).to be_ok
       end
     end
@@ -153,13 +153,13 @@ RSpec.describe HealthCheck, type: :request do
   describe '/heath_check/custom' do
     it 'works with valid custom check' do
       enable_custom_check do
-        get '/health_check/custom'
+        get '/custom_route_prefix/custom'
       end
       expect(response).to be_ok
     end
 
     it 'fails with invalid custom check' do
-      get '/health_check/custom'
+      get '/custom_route_prefix/custom'
       expect(response.status).to eq(550)
       expect(response.body).to include 'health_check failed'
     end
@@ -167,7 +167,7 @@ RSpec.describe HealthCheck, type: :request do
     context 'specified format' do
       it 'returns plain text if client requests html format' do
         enable_custom_check do
-          get '/health_check/custom.html'
+          get '/custom_route_prefix/custom.html'
         end
         expect(response).to be_ok
         expect(response.content_type).to include('text/plain')
@@ -175,22 +175,62 @@ RSpec.describe HealthCheck, type: :request do
 
       it 'returns json if client requests json format' do
         enable_custom_check do
-          get '/health_check/custom.json'
+          get '/custom_route_prefix/custom.json'
         end
         expect(response).to be_ok
         expect(response.content_type).to include('application/json')
         expect(response.parsed_body).to include('healthy' => true, 'message' => 'custom_success_message')
       end
 
+      it 'returns json if client requests json format and custom check is invalid' do
+        get '/custom_route_prefix/custom.json'
+        expect(response.status).to eq(555) # config.http_status_for_error_object = 555
+        expect(response.content_type).to include('application/json')
+        expect(response.parsed_body).to include('healthy' => false)
+      end
+
       it 'returns xml if client requests xml format' do
         enable_custom_check do
-          get '/health_check/custom.xml'
+          get '/custom_route_prefix/custom.xml'
         end
         expect(response).to be_ok
         expect(response.content_type).to include('application/xml')
         expect(response.body).to include('<healthy type="boolean">true</healthy>')
         expect(response.body).to include('<message>custom_success_message</message>')
       end
+
+      it 'returns xml if client requests xml format and custom check is invalid' do
+        get '/custom_route_prefix/custom.xml'
+        expect(response.status).to eq(555) # config.http_status_for_error_object = 555
+        expect(response.content_type).to include('application/xml')
+        expect(response.body).to include('<healthy type="boolean">false</healthy>')
+      end
     end
+  end
+
+  describe '/custom_route_prefix/middleware' do
+    if ENV['MIDDLEWARE'] == 'true'
+      context 'when using middleware' do
+        it 'works with valid custom check' do
+          enable_custom_check do
+            get '/custom_route_prefix/middleware'
+          end
+          expect(response).to be_ok
+        end
+      end
+    end
+    if ENV['MIDDLEWARE'] != 'true'
+      context 'when not using middleware' do
+        it 'fails with invalid custom check' do
+          get '/custom_route_prefix/middleware'
+          expect(response.status).to eq(550)
+          expect(response.body).to include 'health_check failed'
+        end
+      end
+    end
+  end
+
+  context 'whitelisted ip' do
+
   end
 end
