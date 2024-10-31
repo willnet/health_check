4b92931713ec4c116137f94f9da9326673e4d380
Shinichi Maeshima <netwillnet@gmail.com>
2024-10-31 16:52:58 +0900

    昔のRailsではsecret_key_baseの設定が必須だった
    ```
      3) HealthCheck /custom_route_prefix fails with smtp server and invalid custom_check
         Failure/Error: get '/custom_route_prefix'
    
         RuntimeError:
           Missing `secret_key_base` for 'test' environment, set this value in `config/secrets.yml`
         # ./spec/health_check_spec.rb:24:in `block (4 levels) in <top (required)>'
         # ./spec/spec_helper.rb:24:in `start_smtp_server'
         # ./spec/health_check_spec.rb:23:in `block (3 levels) in <top (required)>'
    ```

--- spec/dummy/fake_app.rb
+++ spec/dummy/fake_app.rb
@@ -4,6 +4,7 @@ FakeApp.config.session_store :cookie_store, key: '_myapp_session'
 FakeApp.config.root = File.dirname(__FILE__)
 FakeApp.config.action_mailer.delivery_method = :smtp
 FakeApp.config.action_mailer.smtp_settings = { address: "localhost", port: 3555, openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, enable_starttls_auto: true }
+FakeApp.config.secret_key_base = SecureRandom.hex(64)
 FakeApp.initialize!
 
 
